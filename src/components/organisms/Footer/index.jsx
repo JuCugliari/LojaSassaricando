046368fc4d5499905f14c683ed66d3d202ce74e2
ThaissaCaino <almeida.c.thaissa@gmail.com>
2024-05-05 19:19:57 -0300

    implementação do header and footer

--- src/components/organisms/Footer/index.jsx
+++ src/components/organisms/Footer/index.jsx
@@ -1,21 +1,28 @@
-import "./style.css";
+import "./index.css";
 import "../../../../src/App.css";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+
+import { Grid } from "@mui/material";
+
 function TrilhasFooter() {
  return (
-  <footer className="footer">
-   <span>Sassaricando</span>
-   <ul>
-    <li>
-     <img src="/assets/github-logo-24.png" alt="github" />
-    </li>
-    <li>
-     <img src="/assets/linkedin-logo-24.png" alt="linkedin" />
-    </li>
-    <li>
-     <img src="/assets/instagram-logo-24.png" alt="instagram" />
-    </li>
-   </ul>
-  </footer>
+  <Grid className="containerFooter">
+   <footer className="footer">
+    <ul>
+     <li>
+      <a href="URL-LOJA" target="_blank">
+       <FacebookIcon sx={{ color: "black" }} />
+      </a>
+     </li>
+     <li>
+      <a href="URL-LOJA" target="_blank">
+       <InstagramIcon sx={{ color: "black" }} />
+      </a>
+     </li>
+    </ul>
+   </footer>
+  </Grid>
  );
 }
 
--- src/components/organisms/Header/index.jsx
+++ src/components/organisms/Header/index.jsx
@@ -1,23 +1,75 @@
 /* eslint-disable */
-import { Link } from "react-router-dom";
-import "./style.css";
+import { Button, Menu, MenuItem } from "@mui/material";
+import "./index.css";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import { useContext } from "react";
+import { UsuariosContext } from "../../../context/UsuarioContext";
 
-function TrilhasHeader() {
+function ExerciseOpenAirHeader() {
+ const [anchorEl, setAnchorEl] = useState(null);
+ const navigate = useNavigate();
+ const { logout } = useContext(UsuariosContext);
+
+ const handleClick = (event) => {
+  setAnchorEl(event.currentTarget);
+ };
+
+ const handleClose = () => {
+  setAnchorEl(null);
+ };
+
+ const handleLogout = () => {
+  logout(localStorage.getItem("usuarioLogado"));
+  localStorage.removeItem("usuarioLogado");
+
+  navigate("/");
+  setAnchorEl(null);
+ };
+
  return (
   <div className="header">
-   <div className="logo">
-    <Link to="/">
-     <img src="/assets/logo.png" alt="logo" width={80} height={80} />
-    </Link>
-   </div>
-   <div className="titulo">
-    <span>Sassaricando</span>
-   </div>
-   <div className="subtitulo">
-    <span>Moda Feminina</span>
+   <div className="navbar">
+    <span className="logoHeader">
+     <Link className="labelHome" to="/">
+      <img src="/assets/logo.png" alt="Logo da página" />
+     </Link>
+    </span>
+
+    <span className="logoMeio">
+     <img src="/assets/logo-header.png" alt="Logo do Meio" />
+    </span>
+    <ul className="menuHeader">
+     <li>
+      <Link to="/cadastroLocal">Cadastrar Produtos</Link>
+     </li>
+
+     <li>
+      <Link to="/listaLocal">Listar Produtos</Link>
+     </li>
+     <li>
+      <Button
+       aria-controls="simple-menu"
+       aria-haspopup="true"
+       onClick={handleClick}
+       className="sair">
+       <AccountCircleIcon sx={{ color: "black", fontSize: 35 }} />
+      </Button>
+      <Menu
+       id="simple-menu"
+       anchorEl={anchorEl}
+       keepMounted
+       open={Boolean(anchorEl)}
+       onClose={handleClose}>
+       <MenuItem disabled>{localStorage.getItem("usuarioLogado")}</MenuItem>
+       <MenuItem onClick={handleLogout}>Sair</MenuItem>
+      </Menu>
+     </li>
+    </ul>
    </div>
   </div>
  );
 }
 
-export default TrilhasHeader;
+export default ExerciseOpenAirHeader;
